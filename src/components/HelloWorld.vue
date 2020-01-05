fcb3cd9dd1dc94aa54a24cb78e5394bb228fa873
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-05 16:55:08 +0300

    added firebase implementation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+  <article v-for="(location, idx) in locations" :key="idx">
+    <img :src="location.images" width="100" height="150">
+    <h1>{{ location.name }}</h1>
+    <button @click="deleteLocation(location.id)">
+    Delete
+  </button> 
+  </article>
+
+  <form @submit.prevent="addLocation(names, images)">
+    <input v-model="names" placeholder="Location Name">  <p></p>
+    <input v-model="images" placeholder="Location Image URL">     <p></p>
+    <button type="submit">Add New Location</button>
+</form>
+
+</div>
 </template>
 
 <script>
+import { db } from '../main'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      locations: [],
+      names: '',      // <-- here
+      images: ''      // <-- here
+    }
+  },
+  firestore () {
+    return {
+      locations: db.collection('locations').orderBy('name')
+    }
+  },
+  methods: { 
+    addLocation (names, images) {
+      const created = new Date()
+
+      db.collection('locations').add({ name: names,
+                                       image: images,
+                                       createdAt: created
+                                       })
+      // Clear values
+      
+      this.names = ''
+      this.images = ''
+    },
+
+    deleteLocation (id) {
+      db.collection('locations').doc(id).delete()
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { firestorePlugin } from 'vuefire'
+Vue.use(firestorePlugin)
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCn59gNyStOjUB_GL41nvBMcSyz_Tf-9gk",
+    authDomain: "tmeeducation-report.firebaseapp.com",
+    databaseURL: "https://tmeeducation-report.firebaseio.com",
+    projectId: "tmeeducation-report",
+    storageBucket: "tmeeducation-report.appspot.com",
+    messagingSenderId: "757172656806",
+    appId: "1:757172656806:web:ff795863b45b32ff1fb267",
+    measurementId: "G-CK916K5BQ4"
+});
+export const db = firebaseApp.firestore();
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
